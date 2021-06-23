b7b1aab2f60ac6386a8486bc3f74efc55f56d33d
Erdu-EC <55202417+Erdu-EC@users.noreply.github.com>
2021-06-22 21:28:36 -0600

    Cambios...

--- app/view/Login.php
+++ app/view/Login.php
@@ -19,19 +19,93 @@ if (Session::IsLogin())
 
         <title><?= APP_NAME ?>: Inició de sesión</title>
 
-        <style>
-            main {
-                height: 100vh;
-            }
+        <link rel="stylesheet" href="/files/scss/login.scss">
 
-            main .row {
-                height: 100%
-            }
-        </style>
-
-        <script type="text/javascript" src="/files/js/LoginRegisterPage.js"></script>
     </head>
     <body>
+    <header class="">
+        <nav class="menu-navegacion">
+            <button class="navbar-toggler" id="btn-navbar-toggler">
+                <span class="material-icons">menu</span>
+            </button>
+            <ul class="nav-lista inactivo">
+                <li class="nav-list-item">
+
+                    <a href="/Login" class="nav-link activo"> <span class="material-icons">login</span>Acceder</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="/Register" class="nav-link"><span class="material-icons">
+add
+</span>Resgistrarse</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="#" class="nav-link">
+                        <span class="material-icons">gavel</span>Términos y condiciones</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="#" class="nav-link"><span class="material-icons">
+people_outline
+</span>Sobre Nosotros</a>
+                </li>
+                <li class="nav-list-item">
+                    <a href="#" class="nav-link">
+                        <span class="material-icons"><span class="material-icons-outlined">
+support_agent</span>
+</span>Contacto</a>
+                </li>
+
+
+            </ul>
+
+        </nav>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-7">
+
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-5 contenedor-login" >
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/files/icon/logo.png" alt="">
+                        <?= 'Bienvenido a ' . APP_NAME ?>
+                    </div>
+                    <form id="user_form">
+                        <div class="input-group">
+                            <label for="usuario"><span class="material-icons">person</span></label>
+                            <input type="text" id="user_name" minlength="4" maxlength="30" required placeholder="Introduzca su nombre de usuario">
+                        </div>
+                        <div class="input-group separar">
+                            <label for="clave-usuario"><span class="material-icons">lock</span></label>
+                            <input type="password" id="user_pass"  placeholder="Introduzca su contraseña" minlength="8" maxlength="60" required>
+                        </div>
+                        <div class="recuperar-clave">
+                            <a href="">¿Ha olvidado su contraseña?</a>
+                        </div>
+                        <div class="crear-cuenta">
+                            ¿Todavía no tienes una cuenta Deezer? <a href="/Register">Regístrate</a>
+                        </div>
+                        <div class="input-group">
+                            <input type="submit" value="Acceder" id="acceder">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+    <script type="text/javascript" src="/files/js/LoginPage.js"></script>
+
+
+
+
+
+
+    <!--
     <main class="container-fluid">
         <div class="row">
             <div class="col-sm-3"></div>
@@ -80,7 +154,8 @@ if (Session::IsLogin())
             </div>
             <div class="col-sm-3"></div>
         </div>
-    </main>
+    </main> -->
+    <script type="text/javascript" src="/files/js/Login.js"></script>
     </body>
     </html>
 
---
+++ public/files/js/Login.js
@@ -0,0 +1,49 @@
+
+$("#user_form").submit(function (e){
+    const user = $("#user_name").val();
+    const pass = $("#user_pass").val();
+    const action_alert = $("#action_alert");
+
+    const color_info = "alert-info";
+    const color_error = "alert-danger";
+
+    $.ajax('/Login.json',{
+        method: 'post',
+        dataType: 'json',
+        mimeType: 'application/json',
+        data: {u: user, p: pass},
+        beforeSend: () => action_alert.removeClass(color_error).addClass(color_info).text("Cargando..."),
+        error: () => action_alert.removeClass(color_info).addClass(color_error).text("No fue posible iniciar sesion."),
+        success: function (json){
+            if (json === true){
+                action_alert.removeClass(color_error).addClass(color_info).text('Sesion iniciada.');
+                window.location = "/";
+            }else
+                action_alert.removeClass(color_info).addClass(color_error).text('Usuario o contraseña incorrecta.');
+        }
+    });
+
+    return false;
+});
+
+$(document).on('input', '#user_name', null, function (event){
+    const input = $('#user_name')[0];
+    if (input.validity.tooLong || input.validity.tooShort)
+        input.setCustomValidity("El nombre de usuario debe tener un minimo de 4 caracteres y un maximo de 30.");
+    else
+        input.setCustomValidity('');
+});
+
+$(document).on('input', '#user_pass', null, function (event){
+    const input = $('#user_pass')[0];
+    if (input.validity.tooLong || input.validity.tooShort)
+        input.setCustomValidity("La contraseña debe tener un minimo de 8 caracteres y un maximo de 60.");
+    else
+        input.setCustomValidity('');
+});
+
+
+$("#btn-navbar-toggler").click(function (){
+    $("nav.menu-navegacion ul.nav-lista").toggleClass("activo");
+    $("nav.menu-navegacion ul.nav-lista").toggleClass("inactivo");
+});
