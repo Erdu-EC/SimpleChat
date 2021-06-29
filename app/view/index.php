b6e5d949f0c6ad4e047b981109088330562b21b5
eliezerquintero <eliezerquintero2@gmail.com>
2021-06-29 14:58:26 -0600

    tmp

--- app/view/index.php
+++ app/view/index.php
@@ -12,13 +12,219 @@ use const HS\config\APP_NAME;
     <?php require 'template/Head.php' ?>
 
     <title><?= APP_NAME ?>: Inicio</title>
-
-    <link rel="stylesheet" href="/files/css/chat.css">
-
-    <script type="text/javascript" src="/files/js/Chat.js"></script>
-    <script type="text/javascript" src="/files/js/Conversations.js"></script>
+    <link rel="stylesheet" href="/files/scss/chat.scss">
 </head>
-<body class="d-flex flex-column">
+<body class="d-flex flex-column sb-nav-fixed">
+<!-- Barra superior
+
+<nav class="sb-topnav navbar navbar-expand " id="barra-superior">
+
+    <button class="btn nav-item" id="sidebarToggle">
+        <img src="/files/icon/menu.png" alt="" class="" id="icono-sidebarToggle">
+    </button>
+    <a class="navbar-brand " href="/" >
+        <img src="/files/icon/logo.png" alt="" id="logo-home" class="img img-fluid">
+
+    </a>
+</nav>
+Fin de barra superior -->
+
+<!-- -------------------------------------------------------------
+Contactos cada vez que se vaya a iniciar una nueva conversación
+--------------------------------------------------------------- -->
+<div id="panelTodosContactos">
+    <div id="sidepanelTodosContactos">
+        <div class="titulo-cab no-seleccionable">
+            <h1>Nuevo chat</h1>
+            <div id="ocultar"><span class="material-icons">arrow_back_ios_new</span>
+            </div>
+        </div>
+        <div id="buscar-contacto">
+            <label for="inputBuscarConversacion" class="material-icons">search</i></label>
+            <input name="inputBuscarConversacion" type="text" placeholder="Buscar" maxlength="100"/>
+        </div>
+        <div id="listaTodosContactos">
+            <ul id="listacontactos" class="no-seleccionable">
+
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/rachelzane.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Rachel Zane</h5>
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>rachelzane</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/donnapaulsen.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Donna Paulsen</h5>
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>donnapaulsen</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/jessicapearson.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Jessica Pearson</h5>
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>jessicapearson</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="btn-mas">
+                            <span class="material-icons align-baseline mas">chevron_right</span>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/haroldgunderson.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Harold Gunderson Harold Gunderson</h5>
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>haroldgundersonharoldgunderson</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="btn-mas">
+                            <span class="material-icons align-baseline mas">chevron_right</span>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/danielhardman.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Daniel Hardman</h5>
+
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>danielhardman</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="btn-mas">
+                            <span class="material-icons align-baseline mas">chevron_right</span>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card">
+                        <div class="row align-content-between">
+                            <div class="col-4 perfil-contacto color-6">
+                                <img src="/files/profile/katrinabennett.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Katrina Bennett</h5>
+
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>katrinabennett</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="btn-mas">
+                            <span class="material-icons align-baseline mas ">chevron_right</span>
+                        </div>
+                    </div>
+                </li>
+                <li class="item-contacto">
+                    <div class="card  align-content-between">
+                        <div class="row">
+                            <div class="col-4 perfil-contacto color-5">
+                                <img src="/files/profile/danielhardman.png?w=90&h=90" alt="" class="online"/>
+                            </div>
+                            <div class="col-8 ">
+                                <div class="card-body">
+                                    <h5 class="card-title">Daniel Hardman</h5>
+
+                                    <p class="cont-usuario"><span class="material-icons usuario">person</span>danielhardman</p>
+                                    <div class="btn-opciones">
+
+                                        <span class="material-icons mensaje">message</span>
+                                        <span class="material-icons eliminar">delete</span>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="btn-mas">
+                            <span class="material-icons align-baseline mas">chevron_right</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+
+        </div>
+    </div>
+</div>
+
+<!--Fin del contenedor de los contactos para nuevo chat -->
+
+
+
+<!--
 <header><?php require 'template/Header.php' ?></header>
 
 <main class="container-fluid d-flex flex-grow-1">
@@ -41,16 +247,21 @@ use const HS\config\APP_NAME;
                     </div>
 
                     <ul id="lista-conversaciones" class="list-group list-group-flush">
-                        <!--Las conversaciónes con tus contactos apareceran en esta
+                       Las conversaciónes con tus contactos apareceran en esta
                         lista.-->
+<!--
                     </ul>
                 </div>
             </div>
         </div>
-        <div id="espacio-de-chat" class="col-8">
-            <!-- AQUI SE CARGARA EL HISTORIAL DE CHAT DEL CONTACTO -->
+        <div id="espacio-de-chat" class="col-8"> -->
+            <!-- AQUI SE CARGARA EL HISTORIAL DE CHAT DEL CONTACTO
         </div>
     </div>
+-->
+<script type="text/javascript" src="/files/js/events.js"></script>
+<script type="text/javascript" src="/files/js/Chat.js"></script>
+<script type="text/javascript" src="/files/js/Conversations.js"></script>
 </main>
 </body>
 </html>
---
+++ public/files/js/events.js
@@ -0,0 +1,174 @@
+$(document).on("load", function () {
+    $("#contenido .contact-profile").hide();
+    $("#contenido .messages").hide();
+    $("#contenido .message-input").hide();
+});
+
+
+
+$(document).ready(function (){
+    if($(window).width()<735){
+        $("body").addClass("sb-sidenav-toggled");
+    }
+
+
+});
+$("#sidebarToggle").on("click", function(e) {
+    e.preventDefault();
+    $("body").toggleClass("sb-sidenav-toggled");
+});
+
+$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+
+$("#profile-img").click(function() {
+    $("#status-options").toggleClass("active");
+});
+$(window).resize(function (){
+    if($(window).width()<935){
+        $("body").addClass("sb-sidenav-toggled");
+    }
+    else{
+        $("body").removeClass("sb-sidenav-toggled");
+    }
+});
+
+$("#btn-sesion").click(function (){
+    $("#btn-sesion").toggleClass("btn-activo");
+});
+
+$('.submit').on('click',function() {
+    newMessage();
+});
+
+$(window).on('keydown', function(e) {
+    if (e.which == 13) {
+        newMessage();
+        return false;
+    }
+});
+
+function newMessage() {
+    message = $(".message-input input").val();
+    if($.trim(message) == '') {
+        return false;
+    }
+if($('.messages ul#lista-mensajes').children().last().attr("class")== "enviado"){
+    $('<li class="enviado"><p>' + message + '</p></li>').appendTo($('.messages ul'));
+}
+else {
+    $('<li class="enviado"><img src="/files/profile/mikeross.png?w=40&h=40" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+}
+$('.message-input input').val(null);
+    $('.contact.active .preview').html('<span>You: </span>' + message);
+    $(".messages").animate({ scrollTop: $('.messages').prop("scrollHeight")}, 300);
+
+};
+
+$(".expand-button").click(function() {
+    $("#profile").toggleClass("expanded");
+    //  $("#contacts").toggleClass("expanded");
+});
+
+
+//activar uno de los elementos del menu lateral (seccion actual)
+$('#LateralMenu li').on('click', function(){
+    $('li.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+
+
+//llamar elementos contactos
+$("#seccion-contactos").click(function(){
+    $(location).prop('href', '/Contacts')
+});
+/*
+/*llamar elementos chat*/
+$("#seccion-conversaciones").click(function(){
+    $(location).prop('href', '/')
+});
+$("#ocultar").click(function (){
+
+    $("#panelTodosContactos").animate({
+        margin: "=0 auto 0 -600px"
+    }, {
+        duration: 500,
+        queue: false
+    });
+    if($('body').width() > 1000) {
+        if ($("body").hasClass("prev-inactivo")) {
+            $("body").removeClass("prev-inactivo");
+            $("body").removeClass("sb-sidenav-toggled");
+        }
+    }
+});
+
+$("#nuevo-chat").click(function () {
+    $("#panelTodosContactos").animate({
+        margin: "=0 auto 0 0"
+    }, {
+        duration: 500,
+        queue: false
+    });
+    if($(window).width() > 1000){
+        if (!$("body").hasClass("sb-sidenav-toggled")){
+            $("body").addClass("prev-inactivo");
+        }
+        $("body").addClass("sb-sidenav-toggled");
+    }}
+);
+
+
+$("#btn-enviar-mensaje").click(function() {
+
+    message = $(".message-input input").val();
+    console.log(message);
+    if($.trim(message) == '') {
+        return false;
+    }
+    $('<li class="sent"><img src="/files/upload/profile/mikeross.png?w=40&h=40" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+    $('.message-input input').val(null);
+    $('.contact.active .preview').html('<span>You: </span>' + message);
+    $(".messages").animate({ scrollTop: $('.messages').prop("scrollHeight")}, 300);
+});
+
+
+
+$(".wrap input").bind("keyup keydown change",function () {
+    console.log('activado')
+    message = $(".wrap input").val();
+    if ($.trim(message) == '') {
+        $("#btn-enviar-mensaje").removeClass("activar")
+    }
+    else{
+        $("#btn-enviar-mensaje").addClass("activar")
+    }
+});
+
+$("#btn-info-contacto").click(function (){
+    $("#frame #contenido").addClass("desp-der");
+    $("#panelInfoContacto").addClass("mostrar");
+    $("#btn-info-contacto").addClass("ocultar");
+    if($('#frame .content').width() < 400){
+        $('body').addClass(sb-sidenav-toggled);
+    }
+});
+$("#btn-cerrar-contacto").click(function (){
+    $("#frame #contenido").removeClass("desp-der");
+    $("#panelInfoContacto").removeClass("mostrar");
+    $("#btn-info-contacto").removeClass("ocultar");
+    if($('#frame .content').width()< 400){
+        $('body').removeClass(sb-sidenav-toggled);
+    }
+});
+//detectar tamanos de pantalla y las acciones
+
+$("div#contacts ul#lista-conversaciones").on("click","li.contact",function () {
+
+    $('div#contacts ul#lista-conversaciones li.active').removeClass('active');
+    $(this).addClass("active");
+    $("#contenido .temporal").remove();
+    $("#contenido .contact-profile").show();
+    $("#contenido .messages").show();
+    $("#contenido .message-input").show();
+});
