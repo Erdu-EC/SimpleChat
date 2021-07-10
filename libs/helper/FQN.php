ab9657727f30b1e9b18d054627f73e78f55b6261
Erdu-EC <eduardocastilloh1999@gmail.com>
2021-07-10 11:32:15 -0600

    Arreglando el obtener el nombre completamente cualificado de una clase. - Razon: El serparador de directorios de windows y linux es diferente. W=\    L=/     ClassQualified=\

--- libs/helper/FQN.php
+++ libs/helper/FQN.php
@@ -16,9 +16,10 @@ class FQN
         else {
             foreach (get_required_files() as $file) {
                 if (pathinfo($file)['filename'] == $class) {
-                    $namespace = substr_replace($file, APP_NAMESPACE, 0, strlen(APP_PATH));
-                    $namespace = substr($namespace, 0, strlen($namespace) - 4);
-                    return $namespace;
+                    $namespace = str_replace('/', '\\', $file);
+                    $namespace = substr_replace($namespace, APP_NAMESPACE, 0, strlen(APP_PATH));
+
+                    return substr($namespace, 0, strlen($namespace) - 4);
                 }
             }
 
