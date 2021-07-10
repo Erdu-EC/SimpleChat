c6c70dee12b1d592caa994b6c0b3ccd2794742d4
Erdu-EC <eduardocastilloh1999@gmail.com>
2021-07-10 16:41:35 -0600

    Ajuste a script linux.

--- webpermissions.sh
+++ webpermissions.sh
@@ -9,13 +9,13 @@ cd ..
 sudo chgrp -R www-data ${SERVER_DIR}
 sudo usermod -a -G www-data ${USER}
 
-#Propietario de los ficheros.
-sudo chown -R ${USER} ${SERVER_DIR}
-
 #Permisos de los ficheros y directorios.
 sudo chmod -R 750 ${SERVER_DIR}
 sudo chmod 770 ${SERVER_DIR}
 sudo find ${SERVER_DIR} -type f -exec chmod 740 {} \;
+
+#Propietario de los ficheros.
+sudo chown -R ${USER} ${SERVER_DIR}
 
 #Habilitando modulos.
 sudo a2enmod rewrite
