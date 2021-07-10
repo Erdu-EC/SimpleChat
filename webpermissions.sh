3f7ba4b33c4854c45781381b6d5733d5ff1de3a0
Erdu-EC <eduardocastilloh1999@gmail.com>
2021-07-10 15:43:35 -0600

    Agregando instalación de modulos necesario para linux.

--- webpermissions.sh
+++ webpermissions.sh
@@ -9,15 +9,22 @@ cd ..
 sudo chgrp -R www-data ${SERVER_DIR}
 sudo usermod -a -G www-data ${USER}
 
+#Propietario de los ficheros.
+sudo chown -R ${USER} ${SERVER_DIR}
+
 #Permisos de los ficheros y directorios.
 sudo chmod -R 750 ${SERVER_DIR}
 sudo chmod 770 ${SERVER_DIR}
 sudo find ${SERVER_DIR} -type f -exec chmod 740 {} \;
 
-#Propietario de los ficheros.
-sudo chown -R ${USER} ${SERVER_DIR}
-
 #Habilitando modulos.
 sudo a2enmod rewrite
+
+#Instalando modulos.
+sudo apt-get install php-gd -y
+sudo apt-get install php-mysql -y
+
+#Reiniciando servidor.
+sudo service apache2 restart
 
 echo Terminado
