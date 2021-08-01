79de31a878a33e48fbabffe825467ad1a0a7b088
eliezerquintero <eliezerquintero2@gmail.com>
2021-07-31 22:47:25 -0600

    Configuracion de la cuenta

--- app/view/partial/settings.php
+++ app/view/partial/settings.php
@@ -6,25 +6,111 @@ use HS\libs\core\Session;
 use const HS\config\APP_NAME;
 $SESSION = new Session();
 ?>
-<div class="contenedor-info-cuenta">
+<div class="contenedor-info-cuenta no-seleccionable">
     <div class="row">
-        <div class="col-6">
-            <div class="foto-perfil">
-                <img id="profile-img" src="/files/profile/mikeross?w=100&h=100" class="online" alt="" />
+        <div class="card">
+            <div class="titulo">
+                <span >Perfil</span>
             </div>
 
-        </div>
-        <div class="col-6">
-            <div class="item-perfil"></div>
-            <div class="item-perfil"></div>
-        </div>
+            <div class="guardar" id="btn-guardar-perfil" title="Guardar cambios">
+            <span class="material-icons">
+            save
+            </span>
+            </div>
+            <div class="editar" id="btn-editar-perfil" >
+                <span class="material-icons" title="Editar">edit</span>
+            </div>
+            <form action="">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                        <div class="img-perfil-cuenta">
+                            <img id="foto-perfil-cuenta" src="/files/profile/mikeross.png?w=200&h=200" class="" alt="" />
+                            <div class="opciones">
+  <span class="material-icons">
+photo_camera
+</span>
+                            </div>
+                            <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png">
 
 
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-7 col-xl-7">
+                        <div class="item-perfil-cuenta">
+                            <span class="etiqueta-campo contraida">Nombres</span>
+                            <div id="nombres" class="atributo-perfil">
+                                Michael James
+                            </div>
+
+                        </div>
+                        <div class="item-perfil-cuenta">
+                            <span class="etiqueta-campo contraida">Apellidos</span>
+                            <div id="apellidos" class="atributo-perfil ">
+                                Ross
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="item-perfil-cuenta"></div>
+                        <div class="item-perfil-cuenta"></div></div>
+                </div>
+            </form>
+        </div>
 
     </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="item-perfil"></div>
-            <div class="item-perfil"></div></div>
-    </div>
+   <div class="row">
+       <div class="card conf-acceso">
+
+           <form class="form-conf-acceso">
+               <div class="encabezado">
+                   <span>Configuraciones de  la cuenta</span>
+               </div>
+<div class="checkbox-cambio-clave">
+
+    <input type="checkbox" name="check-cambiar-clave" id="check-cambiar-clave">
+    <label for="check-cambiar-clave">Cambiar contraseña</label>
+</div>
+
+           <div class="row">
+               <div class="col-12">
+                   <div class="item-cuenta">
+                       <span class="etiqueta-campo ">Contraseña anterior</span>
+                       <div id="clave-ant" class="campo-cuenta">
+
+                       </div>
+
+                   </div>
+               </div>
+               <div class="col-12">
+                   <div class="item-cuenta">
+                       <span class="etiqueta-campo ">Nueva contraseña</span>
+                       <div id="clave-nuev" class="campo-cuenta">
+
+                       </div>
+
+                   </div>
+               </div>
+               <div class="col-12">
+                   <div class="item-cuenta">
+                       <span class="etiqueta-campo ">Repita la nueva Contraseña</span>
+                       <div id="clave-nuev-rep" class="campo-cuenta">
+
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+           </form>
+       </div>
+
+   </div>
+
 </div>
--- public/files/js/Settings.js
+++ public/files/js/Settings.js
@@ -3,7 +3,49 @@ $(document).on("click", "#btn-configuraciones", function () {
 });
 
 function CargarEspacioConfiguraciones(){
+    $("body").addClass("sb-sidenav-toggled");
     $('#espacio-de-chat').html(
         ObtenerContenedorHtmlDeAnimacionDeCarga('4.5em', '4.5em', 'text-primary')
     ).load(`/Settings`);
+
 };
+//Evento que se ejecuta al dar click sobre Editar informacion de perfil
+$(document).on("click", "#btn-editar-perfil", function () {
+    $(this).toggleClass("activo");
+    $("#btn-guardar-perfil").toggleClass("activo");
+    $(this).hasClass("activo")?   $("#btn-editar-perfil").html('<span class="material-icons" title="Cancelar">   close</span>'):$("#btn-editar-perfil").html('<span class="material-icons" title="Editar">edit</span>');
+
+    $(".item-perfil-cuenta .atributo-perfil").each(function () {
+       var elemento= $(this);
+       var padre= elemento.parent();
+           if(elemento.attr("contenteditable")=="true"){
+               elemento.attr("contenteditable","false");
+               padre.removeClass("editable");
+
+           }else{
+               elemento.attr("contenteditable","true");
+               padre.addClass("editable");
+
+
+           }
+
+   }
+
+   );
+
+});
+//Evento que se ejecuta al dar click en check para cambiar contrasena
+$(document).on("click", "#check-cambiar-clave", function () {
+    if($(this).val()){
+        $(".item-cuenta .campo-cuenta").each(function () {
+            var elemento= $(this);
+            var padre = $(this).parent();
+            elemento.attr("contenteditable","true");
+            padre.addClass("editable")
+        });
+    }
+});
+$(document).on("click", ".item-cuenta.editable", function (){
+    $(this).children("campo-cuenta").focus();
+    console.log( $(this).children("campo-cuenta").className);
+});
