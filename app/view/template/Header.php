b87b0ead3fb4306ddc574c8bfefd7e39a7bc4899
Erdu-EC <eduardocastilloh1999@gmail.com>
2021-08-27 11:00:32 -0600

    Estilo por defecto.

--- app/view/template/Header.php
+++ app/view/template/Header.php
@@ -1,6 +1,6 @@
 <?php namespace HS\app\view\template;
 
-use HS\libs\core\Session; ?>
+	use HS\libs\core\Session; ?>
 
 
 <nav class="sb-topnav navbar navbar-expand " id="barra-superior">
@@ -10,15 +10,16 @@ use HS\libs\core\Session; ?>
         <span class="material-icons">
 menu
 </span>
-       <!-- <img src="/files/icon/menu.png" alt="" class="" id="icono-sidebarToggle">-->
+        <!-- <img src="/files/icon/menu.png" alt="" class="" id="icono-sidebarToggle">-->
     </button>
-<div class="indicador-mensajes" id="icon-indicador-mensaje">
-    <i class="fas fa-envelope"></i>
-<div class="num-mensajes">  <span>1</span></div>
+    <div class="indicador-mensajes" id="icon-indicador-mensaje">
+        <i class="fas fa-envelope"></i>
+        <div class="num-mensajes" style="display: none">
+            <span>0</span>
+        </div>
+    </div>
 
-</div>
-
-    <a class="navbar-brand " href="/" >
+    <a class="navbar-brand " href="/">
         <img src="/files/icon/logo.png?h=55" alt="" id="logo-home" class="img img-fluid">
 
     </a>
