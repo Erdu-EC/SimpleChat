b48d8cd41789aa01c6bbb310a338c71ad90b59a3
eliezerquintero <eliezerquintero2@gmail.com>
2021-09-14 19:47:25 -0600

    Accesso a MySQL en Google Cloud

--- config/db.php
+++ config/db.php
@@ -10,3 +10,7 @@ const APP_DB_CHARSET = 'utf8mb4';
 class DBAccount{
     const Root = ['user' => 'simplechat', 'pass' => 'simplechat'];
 }
+
+//usuario y clave propuestos (seguros):
+//Usuario: S¡mp1EcH47
+//Contrasena: S¡mp1EcH47_@Dm1n
