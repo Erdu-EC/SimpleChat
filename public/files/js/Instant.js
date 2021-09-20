24deb54b72fc535dc3d57d67e99e2ea09aebf0dd
Erdu-EC <eduardocastilloh1999@gmail.com>
2021-09-20 11:12:51 -0600

    Ajuste a invitaciones inmediatas.

--- public/files/js/Instant.js
+++ public/files/js/Instant.js
@@ -70,8 +70,6 @@ function TratarInvitaciones(inv_list) {
     const lista_conversaciones = $('#lista-conversaciones');
 
     inv_list.forEach(row => {
-        console.log(row);
-
         const elemento = lista_conversaciones.find(`.contact > div[data-usuario=${row.nick}]`);
         const elemento_html = ObtenerElementoConversacion(row.nick, row.first_name, row.last_name, row.profile, null, true, null, row.send_date, row.send_date, row.rcv_date, null);
 
@@ -79,42 +77,13 @@ function TratarInvitaciones(inv_list) {
             $('<li>', {
                 class: 'contact',
                 html: elemento_html
-            }).prependTo(lista_conversaciones);
+            }).prependTo(lista_conversaciones)
         } else {
             //Subirlo a parte superior de lista de conversaciones.
             // y Modificar html existente.
             elemento.parent().html(elemento_html).prependTo(lista_conversaciones);
         }
 
-        /*
-        const elemento_contacto = $(`#lista-conversaciones .contact > div[data-usuario=${row.user_name}]`);
-        const nombre = row.first_name + " " + row.last_name;
-
-        //Si el mensaje es para el contacto de la actual conversación abierta en el chat.
-        if (row.id.toString() === $('#espacio-de-chat .messages').attr('data-usuario'))
-            MostrarMensajeEnEspacioDeChat(nombre, row)
-        else{
-            NotificacionesEscritorio(row.id, nombre, row.content, row.profile);
-
-            //Contar mensajes no leidos.
-            const msg_pendientes = elemento_contacto.find('.num-msj-pendientes.online span');
-
-            if (msg_pendientes.length === 0){
-                elemento_contacto.find('.msj-pendientes').append(obtener_elemento_msg_pendientes(1));
-            }else
-                msg_pendientes.text(parseInt(msg_pendientes.text()) + 1);
-        }
-
-        //Mover conversación hacia arriba en la lista de conversaciónes.
-        elemento_contacto.parent().prependTo($('#lista-conversaciones'));
-
-        //Mostrar vista previa del mensaje en lista de conversaciones.
-        elemento_contacto.find('.hora-ult-mesj').text(Fecha_hora_ultima_Mensaje(row.send_date));
-        elemento_contacto.find('.preview').text(row.content);
-
-        //Actualizar total de conversaciones no leidas.
-        ActualizarTotalDeConversacionesNoLeidas();
-
-         */
+        NotificacionesEscritorio(row.nick, row.first_name + " " + row.last_name, elemento.find('.preview').text(), row.profile);
     })
 }
