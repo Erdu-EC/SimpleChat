a63e26da4e926b454ae089ba37e26c95569d4c5d
eliezerquintero <eliezerquintero2@gmail.com>
2021-11-26 09:47:21 -0600

    Cambios en resgistros de usuarios

--- respaldo-usuarios.sql
+++ respaldo-usuarios.sql
@@ -2,9 +2,9 @@ LOCK TABLES `users` WRITE;
 
 INSERT INTO `users` VALUES
 (8,'marcus','$2y$10$ZwvjHEV01gs6o/SXMimTF.1JAcDtjqlo9F9a3gwLL4FUeUxH1oGCq','Marcos ','López','1996-08-18','M','lopezm.fran1824@gmail.com','88151137','I','2021-10-04 18:12:01',NULL,'marcus.png'),
-(9,'matias_hernandez','$2y$10$4s6S4PvL0ilT9KDo4xyInuptNXE/V3V1bPjRg8KL5ZCUrGvYoB06y','Matías Gabriel','Hernández González','1999-02-10','M','matiasgabrielhernandezgonzalez@gmail.com','81066134','A','2021-10-04 18:47:23','2021-10-11 12:01:20','photo-profile-ma-1.png'),
+(9,'matias_hernandez','$2y$10$4s6S4PvL0ilT9KDo4xyInuptNXE/V3V1bPjRg8KL5ZCUrGvYoB06y','Matías Gabriel','Hernández González','1999-02-10','M','matiasgabrielhernandezgonzalez@gmail.com','81066134','A','2021-10-04 18:47:23','2021-10-11 12:01:20','matias_hernandez.png'),
 (10,'Osman','$2y$10$vkLEYiYUKrmQ4QhLlFx1iucnPeFI3japWC59w9R3G2fTtR1xjUwKy','Osman Rodolfo','Blandon Altamirano ','1998-11-20','M','osman.A201198@gmail.com','85023721','A','2021-10-04 19:03:44','2021-10-05 21:40:34','Osman.jpg'),
-(11,'admin','$2y$10$AeFaSX1.vN8mhJtWD7eTYORFTpph4iqU5AiwLrXfCPlS41z20CWiS','ariel','campos','2006-02-16','D','dfsfsdfds@gmail.com','12355489','A','2021-10-04 19:11:31','2021-10-04 19:51:59','undefined-photo.png'),
+(11,'admin','$2y$10$AeFaSX1.vN8mhJtWD7eTYORFTpph4iqU5AiwLrXfCPlS41z20CWiS','ariel','campos','2006-02-16','D','dfsfsdfds@gmail.com','12355489','A','2021-10-04 19:11:31','2021-10-04 19:51:59','admin.png'),
 (12,'Ernesto ','$2y$10$uv6vYN1ICoF9oGeLDx4.FO5W67wX7OJYHpJIWOBs3HQYYYnlEyfv2','Pelon','Espinoza','2021-10-14','M','1994netoespinoza@gmail.com','86134832','A','2021-10-19 23:12:11','2021-10-19 23:12:29','Ernesto .png'),
 (13,'ToroChingon','$2y$10$MqMrVJ1c6kNTjr05ozaJguzvWNSwMX3van927D/jpXos.gmPZqKhi','toro','chingon','1900-12-24','O','maluma@gmail.com','88888888','A','2021-10-24 12:14:39','2021-10-24 12:14:49','ToroChingon.jpg'),
 (14,'AlexDM26','$2y$10$JSdQF2OsFlCI39sbP6Q3ounqyHGkDL9dy4zyrmApOTk/js1fhm7Uu','Alexander','Delgado','1999-02-26','M','alexander.delgado123@gmail.com','22456665','A','2021-10-24 14:04:05','2021-10-24 14:06:57','AlexDM26.png');
